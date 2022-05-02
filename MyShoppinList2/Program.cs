2f8a9da9c712c50abf3fcc7705a978d73a425e90
Helen Raudsepp <helen.raudsepp@k-print.ee>
2022-05-02 21:53:08 +0300

    add new projects

---
+++ MyShoppinList2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MyShoppinList2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\helen.raudsepp\Samples\myShoppingList";
+            string fileName = @"\\myShoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add something to your shopping list? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter your product:");
+                    string userProduct = Console.ReadLine();
+                    myShoppingList.Add(userProduct);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take care!");
+                }
+            }
+            Console.Clear();
+
+            foreach (string product in myShoppingList)
+            {
+                Console.WriteLine($"Your wish: {product}");
+            }
+            File.WriteAllLines($"{fileLocation}{fileName}", myShoppingList);
+        }
+    }
+}
